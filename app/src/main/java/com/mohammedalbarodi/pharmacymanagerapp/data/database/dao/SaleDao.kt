d579809fbd41823a276cbf210e5091e978cdfce2
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-03 02:17:32 +0300

    Update SaleDao.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/dao/SaleDao.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/dao/SaleDao.kt
@@ -6,7 +6,7 @@ import com.mohammedalbarodi.pharmacymanagerapp.data.model.Sale
 @Dao
 interface SaleDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertSale(sale: Sale)
+    suspend fun insertSale(sale: Sale): Long
 
     @Query("SELECT * FROM sales")
     suspend fun getAllSales(): List<Sale>
