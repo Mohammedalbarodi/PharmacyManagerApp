096c82a0351ac93aa33ec096ce55d89753611588
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-04 02:18:51 +0300

    Add files via upload

---
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/repository/SettingsRepository.kt
@@ -0,0 +1,33 @@
+package com.mohammedalbarodi.pharmacymanagerapp.data.repository
+
+import com.mohammedalbarodi.pharmacymanagerapp.data.database.dao.SettingsDao
+import com.mohammedalbarodi.pharmacymanagerapp.data.database.model.Settings
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class SettingsRepository(private val settingsDao: SettingsDao) {
+
+    suspend fun getSettings(): Settings? {
+        return withContext(Dispatchers.IO) {
+            settingsDao.getSettings()
+        }
+    }
+
+    suspend fun saveSettings(settings: Settings) {
+        withContext(Dispatchers.IO) {
+            settingsDao.insertOrUpdateSettings(settings)
+        }
+    }
+
+    suspend fun resetSettingsToDefault() {
+        withContext(Dispatchers.IO) {
+            val defaultSettings = Settings(
+                id = 1,
+                language = "ar",
+                notificationsEnabled = true,
+                theme = "light"
+            )
+            settingsDao.insertOrUpdateSettings(defaultSettings)
+        }
+    }
+}
