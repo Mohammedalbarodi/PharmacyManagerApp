8f12cb8fd283d34f13d425f353c5d08146ec851d
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-04 03:01:05 +0300

    Update ViewModelFactory.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/ui/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/ui/viewmodel/ViewModelFactory.kt
@@ -4,17 +4,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.mohammedalbarodi.pharmacymanagerapp.data.repository.BackupRepository
 import com.mohammedalbarodi.pharmacymanagerapp.data.repository.MedicineRepository
+import com.mohammedalbarodi.pharmacymanagerapp.data.repository.UserRepository
 
 @Suppress("UNCHECKED_CAST")
 class ViewModelFactory(
     private val medicineRepository: MedicineRepository,
-    private val backupRepository: BackupRepository
+    private val backupRepository: BackupRepository,
+    private val userRepository: UserRepository
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
-                LoginViewModel(medicineRepository) as T
+                LoginViewModel(userRepository) as T
             }
             modelClass.isAssignableFrom(MedicineViewModel::class.java) -> {
                 MedicineViewModel(medicineRepository) as T
@@ -22,7 +24,7 @@ class ViewModelFactory(
             modelClass.isAssignableFrom(SettingsViewModel::class.java) -> {
                 SettingsViewModel(backupRepository) as T
             }
-            // أضف هنا باقي ViewModels عند الحاجة
+            // أضف هنا باقي ViewModels عند الحاجة لاحقًا
             else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
     }
