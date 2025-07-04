44487fdd0fbc6f9cfe2e7d14638b7b3798b126fe
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-04 03:07:05 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/AppDatabase.kt
@@ -1,28 +1,47 @@
 package com.mohammedalbarodi.pharmacymanagerapp.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mohammedalbarodi.pharmacymanagerapp.data.database.dao.ExpenseDao
 import com.mohammedalbarodi.pharmacymanagerapp.data.database.dao.MedicineDao
 import com.mohammedalbarodi.pharmacymanagerapp.data.database.dao.SaleDao
 import com.mohammedalbarodi.pharmacymanagerapp.data.database.dao.UserDao
+import com.mohammedalbarodi.pharmacymanagerapp.data.database.dao.BackupDao
 import com.mohammedalbarodi.pharmacymanagerapp.data.model.Expense
 import com.mohammedalbarodi.pharmacymanagerapp.data.model.Medicine
 import com.mohammedalbarodi.pharmacymanagerapp.data.model.Sale
 import com.mohammedalbarodi.pharmacymanagerapp.data.model.User
+import com.mohammedalbarodi.pharmacymanagerapp.data.model.BackupItem
 
 @Database(
-    entities = [Medicine::class, Expense::class, Sale::class, User::class],
+    entities = [Medicine::class, Expense::class, Sale::class, User::class, BackupItem::class],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun medicineDao(): MedicineDao
-
     abstract fun expenseDao(): ExpenseDao
-
     abstract fun saleDao(): SaleDao
-
     abstract fun userDao(): UserDao
+    abstract fun backupDao(): BackupDao  // 🔹 الجديد
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "pharmacy_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
