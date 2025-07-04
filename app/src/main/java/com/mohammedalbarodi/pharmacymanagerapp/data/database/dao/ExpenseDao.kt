0e44b59e9d140464724caa4a2dddadc2e15d7991
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-04 03:34:35 +0300

    Update ExpenseDao.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/dao/ExpenseDao.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/dao/ExpenseDao.kt
@@ -1,13 +1,24 @@
 package com.mohammedalbarodi.pharmacymanagerapp.data.database.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.mohammedalbarodi.pharmacymanagerapp.data.model.Expense
 
 @Dao
 interface ExpenseDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertExpense(expense: Expense): Long
 
-    @Query("SELECT * FROM expenses")
-    suspend fun getAllExpenses(): List<Expense>
+    @Update
+    suspend fun updateExpense(expense: Expense)
+
+    @Delete
+    suspend fun deleteExpense(expense: Expense)
+
+    @Query("SELECT * FROM expenses ORDER BY date DESC")
+    fun getAllExpenses(): LiveData<List<Expense>>
+
+    @Query("SELECT SUM(amount) FROM expenses")
+    fun getTotalExpenses(): Double
 }
