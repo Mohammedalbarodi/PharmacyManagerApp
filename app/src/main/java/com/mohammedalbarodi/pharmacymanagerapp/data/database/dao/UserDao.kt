bcb6c92f77eb128289777b28a1e38c0ea72f74b5
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-05 23:59:47 +0300

    Update UserDao.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/dao/UserDao.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/database/dao/UserDao.kt
@@ -14,4 +14,16 @@ interface UserDao {
 
     @Query("SELECT * FROM users")
     suspend fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password")
+    suspend fun getUserByUsernameAndPassword(username: String, password: String): User?
+
+    @Query("SELECT * FROM users WHERE username = :username")
+    suspend fun getUserByUsername(username: String): User?
+
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User)
 }
