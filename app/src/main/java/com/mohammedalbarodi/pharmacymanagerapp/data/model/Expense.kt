8da5466c95e261dd45c8b2fd915f9763890c7643
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 02:09:11 +0300

    Update Expense.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Expense.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Expense.kt
@@ -1,3 +1,8 @@
+package com.mohammedalbarodi.pharmacymanager.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "expenses")
 data class Expense(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
