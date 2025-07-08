4e56334793135e33954f4e6f02ae8d2f13cbe242
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 02:10:01 +0300

    Update MedicineCategory.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/MedicineCategory.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/MedicineCategory.kt
@@ -1,3 +1,8 @@
+package com.mohammedalbarodi.pharmacymanager.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "categories")
 data class MedicineCategory(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
