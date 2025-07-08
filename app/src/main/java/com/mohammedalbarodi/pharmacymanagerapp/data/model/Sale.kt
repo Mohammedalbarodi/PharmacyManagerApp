cffc4fa624dbc8984886c9945cf73f41ff632c72
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 01:49:30 +0300

    Update Sale.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Sale.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Sale.kt
@@ -1,13 +1,9 @@
-package com.mohammedalbarodi.pharmacymanagerapp.data.model
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 @Entity(tableName = "sales")
 data class Sale(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val medicineId: Int,
-    val quantitySold: Int,
-    val totalPrice: Double,
-    val date: Long
+    val invoiceNumber: String,
+    val saleDate: Long,
+    val customerId: Int?,
+    val totalAmount: Double,
+    val paymentMethodId: Int
 )
