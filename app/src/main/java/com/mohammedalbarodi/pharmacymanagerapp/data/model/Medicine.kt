80c7c097302e6abba79f6605e42b21c292b916b4
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 01:46:52 +0300

    Update Medicine.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Medicine.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Medicine.kt
@@ -1,12 +1,13 @@
-package com.mohammedalbarodi.pharmacymanagerapp.data.model
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 @Entity(tableName = "medicines")
 data class Medicine(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
+    val brand: String?,
+    val categoryId: Int?,
     val quantity: Int,
-    val price: Double
+    val price: Double,
+    val expiryDate: Long,
+    val barcode: String?,
+    val location: String?,
+    val notes: String?
 )
