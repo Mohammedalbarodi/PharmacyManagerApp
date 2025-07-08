c53af120b53a43f6231eb324510a824394a1d175
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 01:52:48 +0300

    Delete app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/PrescriptionItem.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/PrescriptionItem.kt
+++
@@ -1,8 +0,0 @@
-@Entity(tableName = "prescription_items")
-data class PrescriptionItem(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val prescriptionId: Int,
-    val medicineId: Int,
-    val dosage: String,
-    val duration: String
-)
