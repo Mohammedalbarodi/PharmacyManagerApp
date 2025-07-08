c72858ac7c0d219b3aacc9fe2fde0024f62e2fea
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 02:08:22 +0300

    Update BackupLog.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/BackupLog.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/BackupLog.kt
@@ -1,3 +1,8 @@
+package com.mohammedalbarodi.pharmacymanager.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "backup_logs")
 data class BackupLog(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
