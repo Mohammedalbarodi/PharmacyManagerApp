15973c200a63d57ce6debda08e5ee2e9909fcc67
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 01:58:15 +0300

    Create User.kt

---
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/User.kt
@@ -0,0 +1,8 @@
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val username: String,
+    val password: String,
+    val role: String,
+    val isActive: Boolean
+)
