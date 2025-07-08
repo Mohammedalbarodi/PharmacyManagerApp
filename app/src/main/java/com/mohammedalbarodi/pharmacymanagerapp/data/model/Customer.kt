b8c49cd4d1e57ddcaecd2873d6dd19a10a15446d
Mohammed Albarodi <79165437+Mohammedalbarodi@users.noreply.github.com>
2025-07-09 02:08:46 +0300

    Update Customer.kt

--- app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Customer.kt
+++ app/src/main/java/com/mohammedalbarodi/pharmacymanagerapp/data/model/Customer.kt
@@ -1,3 +1,8 @@
+package com.mohammedalbarodi.pharmacymanager.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "customers")
 data class Customer(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
